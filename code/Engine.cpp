c25ccccfc8f698569ce947fd06f493b53eca5858
Your Name <you@example.com>
2023-11-28 15:52:52 -0800

    Started constructor for Engine.cpp -J

--- code/Engine.cpp
+++ code/Engine.cpp
@@ -1,5 +1,27 @@
-#include "Particle.h"
+#include "Engine.h"
 
 #include <iostream>
 using namespace std;
+
+Engine::Engine() {
+    m_Window.create();
+    VideoMode::getDesktopMode();
+}
+
+void Engine::run() {
+    Clock clock;
+    cout << "Starting Particle unit tests..." << endl;
+    Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
+    p.unitTests();
+    cout << "Unit tests complete.  Starting engine..." << endl;
+
+    while(m_Window.isOpen()) {
+        clock.restart();
+        Time time1 = clock.getElapsedTime();
+        Time time2 = time1.asSeconds();
+        p.input();
+        p.update(time2);
+        p.draw();
+    }
+}
 
